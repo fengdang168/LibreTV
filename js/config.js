55fd232808b5da3185fef31f5bc685c8e4b5a2ee
fengdang168 <fengdang168@gmail.com>
2025-08-17 20:47:06 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,13 +114,11 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-const API_SITES = {
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
         adult: true
-    }
-    //ARCHIVE https://telegra.ph/APIs-08-12
+    },
 };
 
 // 定义合并方法
